9b1fe8808e159fdebf9449d27acc2e1fd1f27b41
pawgli <61730279+pawgli@users.noreply.github.com>
2023-10-28 17:12:36 +0200

    add detekt to the project (#3)

--- app/src/main/kotlin/io/github/pawgli/daggercomposelibrary/ui/theme/Color.kt
+++ app/src/main/kotlin/io/github/pawgli/daggercomposelibrary/ui/theme/Color.kt
@@ -1,3 +1,5 @@
+@file:Suppress("MagicNumber")
+
 package io.github.pawgli.daggercomposelibrary.ui.theme
 
 import androidx.compose.ui.graphics.Color
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,32 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
   alias(libs.plugins.android.app) apply false
   alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.detekt)
+}
+
+dependencies {
+  detektPlugins(libs.detekt.formatting)
+}
+
+detekt {
+  allRules = false
+  autoCorrect = true
+  parallel = true
+  buildUponDefaultConfig = true
+  source.setFrom(projectDir)
+  config.setFrom("$projectDir/detekt.yml")
+}
+
+tasks.withType<Detekt>().configureEach {
+  jvmTarget = libs.versions.java.get()
+  exclude("**/test/**", "**/androidTest/**", "**/build/**")
+  reports {
+    html.required.set(false)
+    xml.required.set(true)
+    txt.required.set(false)
+    sarif.required.set(false)
+    md.required.set(false)
+  }
 }
